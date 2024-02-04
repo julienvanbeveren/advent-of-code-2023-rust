3c9a630aa52d0e508110a387895cd31bb46fb6b2
Julien Van Beveren <julien.vanbeveren@gmail.com>
2024-02-04 15:44:24 +0100

    wip

--- day_03/src/main.rs
+++ day_03/src/main.rs
@@ -1,5 +1,21 @@
-fn part1() {
+use std::fs;
 
+fn part1() {
+    let file = fs::read_to_string("./src/input.txt").expect("Unable to read from file");
+    let lines = file.lines();
+
+    let special_cords: Vec<Vec<&i32>> = [].to_vec();
+
+    let mut lineIndex = -1;
+    for line in lines {
+        lineIndex = lineIndex + 1;
+        let mut wordIndex = -1;
+        let letters = line.split("");
+        for letter in letters {
+            wordIndex = wordIndex + 1;
+            println!("{}", &letter)
+        }
+    }
 }
 
 fn main() {
